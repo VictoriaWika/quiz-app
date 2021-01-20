e997d39113977b67cfac00da7436f3125f664a40
Victoria Wika <victoriawika@icloud.com>
2021-01-20 17:25:46 +0100

    Add storybook

---
+++ components/app-grid.stories.js
@@ -0,0 +1,10 @@
+import '../styles/index.css'
+
+import './app-grid.css'
+import appgrid from './app-grid.html'
+
+export default {
+  title: 'App-grid',
+}
+
+export const base = () => appgrid
---
+++ components/quiz-card.stories.js
@@ -0,0 +1,10 @@
+import '../styles/index.css'
+
+import './quiz-card.css'
+import quizcard from './quiz-card.html'
+
+export default {
+  title: 'Quiz-Card',
+}
+
+export const base = () => quizcard
